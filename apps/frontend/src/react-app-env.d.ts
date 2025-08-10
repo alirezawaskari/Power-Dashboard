ca668651015b4af54e621b7e15f68840deab2f80
Alireza Askari <alirezawaskari@gmail.com>
2025-08-10 16:58:48 +0330

    project rebase update

--- apps/frontend/src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly BASE_URL: string;
-  readonly REACT_APP_API_URL: string;
-  readonly GENERATE_SOURCEMAP: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
